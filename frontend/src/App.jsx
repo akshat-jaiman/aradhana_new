d35162326d3cd1c602951bcfd22a3dfed0560c7c
Akshat <akshatjaiman121@gmail.com>
2026-05-22 10:08:16 +0530

    pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AdmissionsPage from "./pages/AdmissionsPage";
 
-
 function App() {
   return (
-    
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/admissions" element={<AdmissionsPage />} />
     </Routes>
-    
   );
 }
 
