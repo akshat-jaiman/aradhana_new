77e4fcbd9f0d24f8e29521128dfdf2e8d42e5490
Akshat <akshatjaiman121@gmail.com>
2026-05-22 09:47:48 +0530

    pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,10 +4,12 @@ import AdmissionsPage from "./pages/AdmissionsPage";
 
 function App() {
   return (
+    <Router basename="/aradhana_new"> {/* 👈 ADD THIS PROP */}
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/admissions" element={<AdmissionsPage />} />
     </Routes>
+    </Router>
   );
 }
 
